656ee30f490a3b460f3eadb6c9484f1203ed5bfc
Bigperm43 <deanguest43@hotmail.com>
2026-05-09 12:18:48 +1000

    fix: remove site restrictions, open full google search

--- backend/serp.js
+++ backend/serp.js
@@ -1,92 +1,29 @@
 const axios = require("axios");
 
 const COUNTRY_CONFIG = {
-  australia: {
-    gl: "au", hl: "en", google_domain: "google.com.au",
-    sites: "gumtree.com.au OR domain.com.au OR realestate.com.au OR ebay.com.au OR carsales.com.au OR tradingpost.com.au OR facebook.com/marketplace OR caravan.com.au OR bikesales.com.au"
-  },
-  usa: {
-    gl: "us", hl: "en", google_domain: "google.com",
-    sites: "craigslist.org OR ebay.com OR autotrader.com OR cars.com OR zillow.com OR realtor.com OR facebook.com/marketplace OR offerup.com OR mercari.com"
-  },
-  uk: {
-    gl: "gb", hl: "en", google_domain: "google.co.uk",
-    sites: "gumtree.com OR ebay.co.uk OR autotrader.co.uk OR rightmove.co.uk OR zoopla.co.uk OR facebook.com/marketplace OR preloved.co.uk"
-  },
-  canada: {
-    gl: "ca", hl: "en", google_domain: "google.ca",
-    sites: "kijiji.ca OR ebay.ca OR autotrader.ca OR realtor.ca OR facebook.com/marketplace"
-  },
-  "new zealand": {
-    gl: "nz", hl: "en", google_domain: "google.co.nz",
-    sites: "trademe.co.nz OR ebay.com.au OR gumtree.com.au OR facebook.com/marketplace"
-  },
-  germany: {
-    gl: "de", hl: "de", google_domain: "google.de",
-    sites: "ebay-kleinanzeigen.de OR ebay.de OR mobile.de OR immobilienscout24.de OR facebook.com/marketplace"
-  },
-  france: {
-    gl: "fr", hl: "fr", google_domain: "google.fr",
-    sites: "leboncoin.fr OR ebay.fr OR lacentrale.fr OR facebook.com/marketplace"
-  },
-  italy: {
-    gl: "it", hl: "it", google_domain: "google.it",
-    sites: "subito.it OR ebay.it OR autoscout24.it OR facebook.com/marketplace"
-  },
-  spain: {
-    gl: "es", hl: "es", google_domain: "google.es",
-    sites: "wallapop.com OR milanuncios.com OR ebay.es OR facebook.com/marketplace"
-  },
-  japan: {
-    gl: "jp", hl: "ja", google_domain: "google.co.jp",
-    sites: "mercari.com OR yahoo.co.jp OR ebay.com OR facebook.com/marketplace"
-  },
-  russia: {
-    gl: "ru", hl: "ru", google_domain: "google.ru",
-    sites: "avito.ru OR auto.ru OR ebay.com OR facebook.com/marketplace"
-  },
-  netherlands: {
-    gl: "nl", hl: "nl", google_domain: "google.nl",
-    sites: "marktplaats.nl OR ebay.nl OR facebook.com/marketplace"
-  },
-  sweden: {
-    gl: "se", hl: "sv", google_domain: "google.se",
-    sites: "blocket.se OR ebay.com OR facebook.com/marketplace"
-  },
-  norway: {
-    gl: "no", hl: "no", google_domain: "google.no",
-    sites: "finn.no OR ebay.com OR facebook.com/marketplace"
-  },
-  denmark: {
-    gl: "dk", hl: "da", google_domain: "google.dk",
-    sites: "dba.dk OR ebay.com OR facebook.com/marketplace"
-  },
-  singapore: {
-    gl: "sg", hl: "en", google_domain: "google.com.sg",
-    sites: "carousell.sg OR ebay.com OR facebook.com/marketplace"
-  },
-  india: {
-    gl: "in", hl: "en", google_domain: "google.co.in",
-    sites: "olx.in OR quikr.com OR ebay.in OR facebook.com/marketplace"
-  },
-  brazil: {
-    gl: "br", hl: "pt", google_domain: "google.com.br",
-    sites: "olx.com.br OR mercadolivre.com.br OR facebook.com/marketplace"
-  },
-  mexico: {
-    gl: "mx", hl: "es", google_domain: "google.com.mx",
-    sites: "mercadolibre.com.mx OR segundamano.mx OR facebook.com/marketplace"
-  },
-  uae: {
-    gl: "ae", hl: "en", google_domain: "google.ae",
-    sites: "dubizzle.com OR opensooq.com OR ebay.com OR facebook.com/marketplace"
-  },
-  "south africa": {
-    gl: "za", hl: "en", google_domain: "google.co.za",
-    sites: "gumtree.co.za OR autotrader.co.za OR property24.com OR facebook.com/marketplace"
-  },
-  global: { gl: "us", hl: "en", google_domain: "google.com", sites: null },
-  any: { gl: "us", hl: "en", google_domain: "google.com", sites: null },
+  australia: { gl: "au", hl: "en", google_domain: "google.com.au" },
+  usa: { gl: "us", hl: "en", google_domain: "google.com" },
+  uk: { gl: "gb", hl: "en", google_domain: "google.co.uk" },
+  canada: { gl: "ca", hl: "en", google_domain: "google.ca" },
+  "new zealand": { gl: "nz", hl: "en", google_domain: "google.co.nz" },
+  germany: { gl: "de", hl: "de", google_domain: "google.de" },
+  france: { gl: "fr", hl: "fr", google_domain: "google.fr" },
+  italy: { gl: "it", hl: "it", google_domain: "google.it" },
+  spain: { gl: "es", hl: "es", google_domain: "google.es" },
+  japan: { gl: "jp", hl: "ja", google_domain: "google.co.jp" },
+  russia: { gl: "ru", hl: "ru", google_domain: "google.ru" },
+  netherlands: { gl: "nl", hl: "nl", google_domain: "google.nl" },
+  sweden: { gl: "se", hl: "sv", google_domain: "google.se" },
+  norway: { gl: "no", hl: "no", google_domain: "google.no" },
+  denmark: { gl: "dk", hl: "da", google_domain: "google.dk" },
+  singapore: { gl: "sg", hl: "en", google_domain: "google.com.sg" },
+  india: { gl: "in", hl: "en", google_domain: "google.co.in" },
+  brazil: { gl: "br", hl: "pt", google_domain: "google.com.br" },
+  mexico: { gl: "mx", hl: "es", google_domain: "google.com.mx" },
+  uae: { gl: "ae", hl: "en", google_domain: "google.ae" },
+  "south africa": { gl: "za", hl: "en", google_domain: "google.co.za" },
+  global: { gl: "us", hl: "en", google_domain: "google.com" },
+  any: { gl: "us", hl: "en", google_domain: "google.com" },
 };
 
 const EXCLUDE_SITES = [
@@ -97,7 +34,6 @@ const EXCLUDE_SITES = [
 
 const GLOBAL_COUNTRIES = ["australia", "usa", "uk", "canada", "new zealand"];
 
-// Source favicon map for clean fallback images
 const SOURCE_ICONS = {
   "ebay.com": "https://ir.ebaystatic.com/pictures/aw/pics/favicon.ico",
   "ebay.com.au": "https://ir.ebaystatic.com/pictures/aw/pics/favicon.ico",
@@ -117,11 +53,9 @@ function extractDomain(url) {
 
 function getFallbackImage(url) {
   const domain = extractDomain(url);
-  // Check known sources first
   for (const [key, icon] of Object.entries(SOURCE_ICONS)) {
     if (domain.includes(key)) return icon;
   }
-  // Generic Google favicon fallback
   return `https://www.google.com/s2/favicons?domain=${domain}&sz=128`;
 }
 
@@ -144,7 +78,6 @@ async function search({ query, priceMin, priceMax, country = "any", mode = "glob
       }
     }
 
-    // Deduplicate by URL
     const seen = new Set();
     const deduped = allResults.filter(r => {
       if (seen.has(r.listingUrl)) return false;
@@ -177,10 +110,6 @@ async function searchSingle({ query, priceMin, priceMax, country = "any", mode =
     fullQuery += ` -site:${site}`;
   }
 
-  if (mode === "local" && config.sites) {
-    fullQuery += ` (site:${config.sites.replace(/ OR /g, " OR site:")})`;
-  }
-
   console.log("[serp] Query: " + fullQuery);
   console.log("[serp] Country: " + countryKey + " | Mode: " + mode);
 
@@ -200,7 +129,6 @@ async function searchSingle({ query, priceMin, priceMax, country = "any", mode =
     console.log("[serp] Got " + items.length + " results from " + countryKey);
 
     return items.map((item, i) => {
-      // Try every possible image field SerpAPI might return
       const imageUrl =
         item.thumbnail ||
         item.rich_snippet?.top?.extensions?.[0] ||
